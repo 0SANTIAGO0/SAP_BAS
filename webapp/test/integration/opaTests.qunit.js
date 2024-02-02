95b2951e8eb2b0c197d670c2cd7a142bf174804d
0SANTIAGO0 <lleddysantiagoll@outlook.com>
2024-02-02 01:30:28 +0000

    First Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ns/aprobacionesentel/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/aprobacionesentel/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
